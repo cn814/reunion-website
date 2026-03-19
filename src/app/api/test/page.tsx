ef7ab7bc920f64fe5d18013ba4df1b44dc9c2aa8
Chris Nealen <christopher.nealen@rtx.com>
2026-03-18 22:50:41 -0400

    feat: create a new test page at `/api/test` using Edge runtime.

---
+++ src/app/api/test/page.tsx
@@ -0,0 +1,11 @@
+export const runtime = 'edge';
+
+export default function TestPage() {
+  return (
+    <html>
+      <body>
+        <h1>Minimal Page Success</h1>
+      </body>
+    </html>
+  );
+}
--- src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-export const runtime = 'edge';
-
-export async function GET() {
-  return new Response('{"message": "Strictly Zero Import Success"}', {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
